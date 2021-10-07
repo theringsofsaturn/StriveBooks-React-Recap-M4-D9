835bbba7b40cee2ba761fa6a93ffef2f6ed339b4
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-10-07 17:06:17 +0200

    messed project

--- strive-books/strive-books/src/App.js
+++ strive-books/strive-books/src/App.js
@@ -2,14 +2,18 @@ import "./App.css";
 import fantasy from "./Data/fantasy.json";
 import BookList from "./Components/BookList";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Registration from "./Components/Registration";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Route path="/" exact render={() => <BookList books={fantasy} />} />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Route path="/" exact render={() => <BookList books={fantasy} />} />
+          <Route path="/register" exact component={Registration} />
+        </header>
+      </div>
+    </Router>
   );
 }
 
